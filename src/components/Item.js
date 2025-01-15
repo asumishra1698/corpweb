827c0356716b09e226bcc90bfb8e8a90fb008708
Asutosh Mishra <asumishra1698@gmail.com>
2025-01-16 00:26:07 +0530

    Tailwind integration

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,6 @@
 import "./Item.css";
 function Item(props) {
   const name = props.name;
-  return <p className="nirma">{name}</p>;
+  return <p className="bg-blue-500">{name}</p>;
 }
 export default Item;
