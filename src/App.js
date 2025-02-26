1ceb672a277cc548f03e379536860346df8a867e
Asutosh Mishra <asumishra1698@gmail.com>
2025-02-26 23:29:11 +0530

    okok

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import Item from "./components/Item";
-import "./App.css";
+import { useState } from 'react';
 
-function App() {
-  const name1 = "Ankita Tiwari"
-  const name2 = "Shivam Tiwari"
-  const name3 = "Preeti Tiwari"
+const initialItems = [
+  { title: 'pretzels', id: 0 },
+  { title: 'crispy seaweed', id: 1 },
+  { title: 'granola bar', id: 2 },
+];
+
+export default function Menu() {
+  const [items, setItems] = useState(initialItems);
+  const [selectedItem, setSelectedItem] = useState(
+    items[0]
+  );
+
+  function handleItemChange(id, e) {
+    setItems(items.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          title: e.target.value,
+        };
+      } else {
+        return item;
+      }
+    }));
+  }
+
   return (
-    <div>
-      <Item name={name1}></Item>
-      <Item name={name2}></Item>
-      <Item name={name3}></Item>
-      <div className="App">hello ji</div>
-    </div>
+    <>
+      <h2>What's your travel snack?</h2> 
+      <ul>
+        {items.map((item, index) => (
+          <li key={item.id}>
+            <input
+              value={item.title}
+              onChange={e => {
+                handleItemChange(item.id, e)
+              }}
+            />
+            {' '}
+            <button onClick={() => {
+              setSelectedItem(item);
+            }}>Choose</button>
+          </li>
+        ))}
+      </ul>
+      <p>You picked {selectedItem.title}.</p>
+    </>
   );
 }
-
-export default App;
